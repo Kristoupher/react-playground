7ca8d909b14344e4f6bc78794dd5b12ef3eb3e3e
Kristoupher <kri.arregui@gmail.com>
2022-11-14 15:29:08 +0100

    exo3

--- main.js
+++ main.js
@@ -1,5 +1,13 @@
-//const helloWorld = React.createElement('h1', {}, 'Hello world!');
-
 const helloWorld = <h1>Hello world!</h1>;
+const lastName = "Arregui";
+const firstName = "Kristopher";
 
-ReactDOM.render(helloWorld, document.querySelector('#app'));
+const result = (
+  <h1>
+    {helloWorld}{" "}
+    <span style={{ color: "red", textTransform: "uppercase" }}>{lastName}</span>{" "}
+    {firstName[0].toUpperCase() + firstName.slice(1)}
+  </h1>
+);
+
+ReactDOM.render(result, document.querySelector("#app"));
